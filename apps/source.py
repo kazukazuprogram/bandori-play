12faf89532b3df2ecf6b6b83ff0828309b106803
kazukazuprogram <None>
2019-09-10 18:18:23 +0900

    first manual commit

---
+++ apps/source.py
@@ -0,0 +1,284 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from requests import Session
+from bs4 import BeautifulSoup as bs
+from os import environ
+from os.path import join, isdir
+from os import makedirs as mkdir
+from json import loads
+from os import system
+from sys import argv
+from subprocess import check_output
+from sys import stderr
+import winreg
+
+baseurl = "https://bandori.fandom.com"
+downloadBasePath = join(environ["homepath"], "BandoriWiki")
+artistCorresp = {
+"Hello, Happy World!": "ハロー、ハッピーワールド!",
+"Roselia": "Roselia",
+"Poppin'Party": "Poppin'Party"
+}
+
+def get_proxy():
+    if "--proxy" in argv:
+        try:
+            p = argv[argv.index("--proxy")+1]
+            global_proxy = {
+            "http": p,
+            "https": p
+            }
+            print("Option  : Using proxy \""+p+"\"")
+        except IndexError:
+            print("Warning: The proxy has been disabled because the \"--proxy\" option was specified but no value was entered")
+    elif "--disable-proxy" in argv:
+        return None
+    elif "http_proxy" in [x.lower() for x in list(environ.keys())]:
+        global_proxy = {
+        "http": environ["http_proxy"].replace("http://", "").replace("/", ""),
+        "https": environ["https_proxy"].replace("https:", "").replace("http:", "").replace("/", "")
+        }
+        print("Option  : Using proxy", str(global_proxy), "(environ)")
+    elif "https_proxy" in [x.lower() for x in list(environ.keys())]:
+        global_proxy = {
+        "http": environ["https_proxy"].replace("http://", "").replace("/", ""),
+        "https": environ["https_proxy"].replace("https://", "").replace("/", "")
+        }
+        print("Option  : Using proxy \""+global_proxy["http"]+"\" (environ)")
+    elif False: # netshでプロキシの存在を確認できないため将来的に実装予定
+        p = check_output("netsh winhttp show proxy").decode("sjis").replace("\r\n", "\n").split("\n")[3].split(":", 1)[1].replace(" ", "")
+        global_proxy = {
+        "http": p,
+        "https": p
+        }
+        print("Option  : Using proxy \"" + global_proxy["http"] + "\" (netsh winhttp)")
+    else:
+        path = r"Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+        key = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, path)
+        p, regtype = winreg.QueryValueEx(key, 'ProxyServer')
+        e, regtype = winreg.QueryValueEx(key, 'ProxyEnable')
+        e = bool(e)
+        winreg.CloseKey(key)  # key.Close() と書いても同じ
+        if bool(e):
+            global_proxy = {
+            "http": p,
+            "https": p
+            }
+            print("Option  : Using proxy \""+global_proxy["http"]+"\" (System Setting)")
+        else:
+            global_proxy = None
+    return global_proxy
+
+# 楽曲一覧を取得
+def getSongList(s=Session()):
+    g = s.get(baseurl+"/wiki/BanG_Dream!_Girls_Band_Party!/Playable_Songs", proxies=global_proxy)
+    f = bs(g.text, "lxml")
+    f = f.find("div", class_="mw-content-text").find_all("table")
+    l = list()
+    for x in f:
+        lt = list()
+        for y in x.find_all("a"):
+            lt.append({
+            "title": y.get("title"),
+            "url": y.get("href"),
+            })
+        l += lt
+    return l
+
+def getSongInfo(url, s=Session()):
+    g = s.get(baseurl+url, proxies=global_proxy)
+    f = bs(g.text, "lxml")
+    audio = list()
+    for x in f.find("table", class_="article-table").find_all("tr")[1:]:
+        t = x.find_all("td")
+        title = t[1].text.replace("\n","")
+        audio.append({
+        "title": title,
+        "length": t[2].text.replace("\n", ""),
+        "url": x.find("span", class_="ogg_custom").button.get("onclick").split("rl\":\"")[1].split("\",\"")[0],
+        "inst": "instrumental" in title,
+        "gamever": "(Game Version)" in title
+        })
+        del title
+    try:
+        title = f.find("div", class_="mw-content-text").i.span.text
+    except:
+        try:
+            title = f.find("div", class_="pi-data-value pi-font").text
+        except:
+            title = f.find("div", class_="page-header__main").h1.text
+    try:
+        artist = artistCorresp[f.find("div", class_="mw-content-text").p.find_all("a")[-1].text]
+    except:
+        artist = f.find("div", class_="mw-content-text").p.find_all("a")[-1].text
+    try:
+        bpm = int(f.find("div", class_="mw-content-text").find("div", style="float:left;")\
+            .find_all("table")[2].tr.find_all("td")[1].text.replace("\n", "").replace(" BPM", ""))
+    except:
+        try:
+            bpm = f.find("div", class_="mw-content-text").find("div", style="float:left;")\
+                .find_all("table")[2].tr.find_all("td")[1].text.replace("\n","").replace(" BPM","").replace(" ~ ","-")
+        except:
+            bpm = "Unacquirable"
+            stderr.write("Error: BPM unacquirable.\n")
+    info = {
+    "title": title,
+    "artwork": f.find("a", class_="image-thumbnail").get("href"),
+    "bpm": bpm,
+    "audio": audio,
+    "artist": artist
+    }
+    return info
+
+def suggestSearch(q, s=Session()):
+    g = s.get(baseurl+"/index.php", params={
+        "action": "ajax",
+        "rs": "getLinkSuggest",
+        "format": "json",
+        "query": q
+    }, proxies=global_proxy)
+    l = loads(g.text)
+    return l["suggestions"]
+
+def searchAudio(q, s=Session()):
+    if q == "q" or q is None or len(q) == 0:
+        return False
+    sug = suggestSearch(q=q, s=s)
+    ss = True
+    if len(sug) != 0:
+        n = 0
+        for x in sug:
+            print(n, "\t"+x)
+            n += 1
+    else:
+        if s.get(baseurl+"/wiki/"+q).status_code != 404:
+            r = "/wiki/"+q
+            ss = False
+        else:
+            print("No hit.")
+            searchAudio(q=input("search>"), s=s)
+    if ss:
+        i = input("Whitch?[0] : ")
+        if len(i) == 0:
+            i = 0
+        else:
+            i = int(i)
+        r = "/wiki/"+sug[i]
+    info = getSongInfo(r, s=s)
+    print("Title : \t", info["title"])
+    print("Artist :\t", info["artist"])
+    return info
+
+def playAudio(d):
+    global global_proxy
+    if global_proxy is None:
+        _global_proxy = None
+    else:
+        _global_proxy = global_proxy.copy()
+    if global_proxy is not None:
+        if global_proxy["http"][:7] != "http://":
+            global_proxy["http"] = "http://" + global_proxy["http"]
+        environ["http_proxy"] = global_proxy["http"]
+        if global_proxy["https"][:8] != "https://":
+            global_proxy["https"] = "https://" + global_proxy["https"]
+        environ["https_proxy"] = global_proxy["https"]
+    print("Title : \t", d["data"]["title"])
+    print("Artist :\t", d["data"]["artist"])
+    n = 0
+    for x in d["data"]["audio"]:
+        print(n, "\t"+x["title"])
+        n += 1
+    i = input("Whitch?[0] : ")
+    if len(i) == 0:
+        i = 0
+    else:
+        i = int(i)
+    system("start cmd /c bin\\ffplay "+d["data"]["audio"][i]["url"]+" -loop 0")
+    global_proxy = _global_proxy
+
+def _console(i=None):
+    global current
+    if i is None:
+        i = input("bandoriwiki>").split(" ")
+    while "" in i:
+        i.remove("")
+    if len(i) == 0:
+        return False
+    elif i[0].lower() in ["search", "s"]:
+        if len(i) >= 2:
+            q = " ".join(i[1:])
+        else:
+            q = input("search>")
+        current["data"] = searchAudio(q=q)
+        current["set"] = True
+    elif i[0].lower() in ["play", "p"]:
+        if not current["set"]:
+            print("Error: Please set current song.")
+        else:
+            playAudio(current)
+    elif i[0].lower() in ["download", "d"]:
+        if not current["set"]:
+            print("Error: Please set current song.")
+        else:
+            if len(i) == 1:
+                n = None
+            else:
+                try:
+                    n = int(i[1])
+                except:
+                    n = None
+            downloadAudio(current, num=n)
+    elif i[0].lower() == "print":
+        print(global_proxy)
+    elif i[0].lower() == "clear":
+        current = {"set": False, "data": {}}
+    elif i[0].lower() in ["quit", "exit", "q"]:
+        return True
+    else:
+        print("Unknown command:", i[0])
+
+def downloadAudio(d, num=None, s=Session()):
+    print("Title : \t", d["data"]["title"])
+    print("Artist :\t", d["data"]["artist"])
+    n = 0
+    for x in d["data"]["audio"]:
+        print(n, "\t"+x["title"])
+        n += 1
+    while True:
+        try:
+            i = input("Whitch?[0] : ")
+            if len(i) == 0:
+                i = 0
+            elif i.lower() == "q":
+                return
+            else:
+                i = int(i)
+            break
+        except:
+            print("Please enter number")
+    print("Download :", d["data"]["audio"][i]["url"])
+    path = downloadBasePath
+    if not isdir(path):
+        mkdir(path)
+    filename = d["data"]["title"].replace(" ", "_")
+    if d["data"]["audio"][i]["gamever"]:
+        filename += "_gamever"
+    elif d["data"]["audio"][i]["inst"]:
+        filename += "_inst"
+    filename += ".ogg"
+    with open(join(path, filename), "wb") as fp:
+        with s.get(d["data"]["audio"][i]["url"], proxies=global_proxy) as g:
+            fp.write(g.content)
+
+def console():
+    global current
+    current = {"set": False, "data": {}}
+    while True:
+        cr = _console()
+        if cr:
+            return cr
+
+if __name__ == "__main__":
+    global_proxy = get_proxy()
+    console()
