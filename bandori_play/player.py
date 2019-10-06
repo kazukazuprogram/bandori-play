177be2da6927435400cf30929a8af664d4583d6a
kazukazuprogram <None>
2019-10-07 01:00:13 +0900

    A

--- bandori_play/player.py
+++ bandori_play/player.py
@@ -1,7 +1,9 @@
 from os import environ, system
 from os.path import join, split
-ffplay_path = join(split(__file__)[0], "bin", "ffplay.exe")
+from subprocess import Popen, DEVNULL
+from threading import Thread
 
+ffplay_path = join(split(__file__)[0], "bin", "ffplay.exe")
 
 class Player():
     def __init__(self, url=None, proxy=None):
@@ -9,6 +11,7 @@ class Player():
         self.ps = None
         self.playing = False
         self.loop = False
+        self.proxy = proxy
 
     def setURL(self, url):
         self.url = url
@@ -29,18 +32,15 @@ class Player():
         self.command = ffplay_path + \
             " -i {} -nodisp -autoexit".format(self.url)
         envcopy = environ.copy()
-        if global_proxy is not None:
-            envcopy["http_proxy"] = global_proxy["http"]
-            envcopy["https_proxy"] = global_proxy["https"]
-        if play_legacy:
-            system(self.command)
-        else:
-            self.ps = Popen(self.command.split(), stdout=DEVNULL,
-                            stderr=DEVNULL, env=envcopy)
-            self.wait = self.ps.wait
-            self.playing = True
-            self.waitThread = Thread(target=self.waitFunc)
-            self.waitThread.start()
+        if self.proxy is not None:
+            envcopy["http_proxy"] = self.proxy["http"]
+            envcopy["https_proxy"] = self.proxy["https"]
+        self.ps = Popen(self.command.split(), stdout=DEVNULL,
+                        stderr=DEVNULL, env=envcopy)
+        self.wait = self.ps.wait
+        self.playing = True
+        self.waitThread = Thread(target=self.waitFunc)
+        self.waitThread.start()
 
     def stop(self):
         if not self.ps:
